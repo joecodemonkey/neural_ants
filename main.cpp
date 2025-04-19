e8649e336627a3615e7b0fa41ce61cb097e3bd9f
Joe Hurdle <joehurdle@gmail.com>
2025-04-19 18:15:51 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "raylib-cpp.hpp"
+#include <format>
+#include <string>
+
+int main(void) {
+
+    int screenWidth = 800;
+    int screenHeight = 450;
+    int fontSize = 20;
+
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+
+
+    std::string text = std::format("Window Size: {} x {}", screenWidth, screenHeight);
+    auto textSize = MeasureTextEx(GetFontDefault(), text.c_str(), fontSize, 1.0f);
+    int textX = (screenWidth - textSize.x) / 2;
+    int textY = (screenHeight - textSize.y) / 2;
+
+    const float desiredTextWidthRatio = static_cast<float>(textSize.x) / screenWidth;
+
+    SetTargetFPS(60);
+
+    while (!window.ShouldClose())
+    {
+        if (window.IsResized())
+        {
+            screenWidth = window.GetWidth();
+            screenHeight = window.GetHeight();
+            textX = (screenWidth - textSize.x) / 2;
+            textY = (screenHeight - textSize.y) / 2;
+            text = std::format("Window Size: {} x {}", screenWidth, screenHeight);
+        }
+
+        BeginDrawing();
+
+        window.ClearBackground(RAYWHITE);
+
+        DrawText(text.c_str(), textX, textY, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    // UnloadTexture() and CloseWindow() are called automatically.
+
+    return 0;
+
+}
