1ad38dc9d4eec361e50ecc8b63f39ecebbc1d53e
Joe Hurdle <joehurdle@gmail.com>
2025-04-25 20:07:33 -0500

    pan and zoom with mouse and keyboard work much better.

--- main.cpp
+++ main.cpp
@@ -30,10 +30,40 @@ int main(void) {
 
     float adjust = 1.0f;
     while (!window.ShouldClose()) {
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) camera.offset = Vector2Add(camera.offset, GetMouseDelta());
 
         if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)) {
-            if (IsKeyDown('=') || IsKeyDown(KEY_KP_ADD)) camera.zoom += 0.01f;
-            if (IsKeyDown('-') || IsKeyDown(KEY_KP_SUBTRACT)) camera.zoom -= 0.01f;
+            float zoom_adj = 0.0f;
+
+            if (IsKeyDown('=') || IsKeyDown(KEY_KP_ADD)) zoom_adj = 0.01f;
+            if (IsKeyDown('-') || IsKeyDown(KEY_KP_SUBTRACT)) zoom_adj = -0.01f;
+            if (zoom_adj != 0.0f) {
+                float oldZoom = camera.zoom;
+                camera.zoom += zoom_adj;
+                // Adjust offset to keep the same point under the cursor
+                Vector2 mousePos = GetMousePosition();
+                Vector2 worldPos = GetScreenToWorld2D(mousePos, camera);
+                camera.offset = Vector2Add(camera.offset, Vector2Scale(Vector2Subtract(worldPos, camera.target), (camera.zoom - oldZoom) / oldZoom));
+            }
+
+            auto mouse_wheel_move = GetMouseWheelMove();
+            if (mouse_wheel_move != 0)
+            {
+                // Get the world point that is under the mouse
+                Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+                // Set the offset to where the mouse is
+                camera.offset = GetMousePosition();
+
+                // Set the target to match, so that the camera maps the world space point 
+                // under the cursor to the screen space point under the cursor at any zoom
+                camera.target = mouseWorldPos;
+
+                // Zoom increment
+                // Uses log scaling to provide consistent zoom speed
+                float scale = 0.2f*mouse_wheel_move;
+                camera.zoom = Clamp(expf(logf(camera.zoom)+scale), 0.125f, 64.0f);
+            }
         }
 
         adjust = 1.0f;
