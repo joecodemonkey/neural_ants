476795a04f1860da008720b519f8467f1d34d8d5
Joe Hurdle <joehurdle@gmail.com>
2025-05-24 12:55:50 -0500

    Moved to Modern C++ method signatures for world and added concept of spawn bounds

--- world.cpp
+++ world.cpp
@@ -8,7 +8,9 @@
 
 World::World() : _resources(*this) {
   _bounds = World::DEFAULT_BOUNDS;
+  _spawnMargin = DEFAULT_SPAWN_MARGIN;
   _population.set_world_size({_bounds.width, _bounds.height});
+  update_spawn_rect();
 }
 
 auto World::get_bounds() const -> const Rectangle& {
@@ -31,14 +33,41 @@ auto World::get_resources() const -> const Resources& {
   return _resources;
 }
 
-void World::update(float time) {
+auto World::get_spawn_bounds() const -> const Rectangle& {
+  return _spawnBounds;
+}
+
+auto World::set_spawn_margin(float margin) -> void {
+  _spawnMargin = margin;
+  update_spawn_rect();
+}
+
+auto World::get_spawn_margin() const -> float {
+  return _spawnMargin;
+}
+
+auto World::update_spawn_rect() -> void {
+  float margin = _bounds.width * _spawnMargin;
+  _spawnBounds = {_bounds.x + margin,
+                  _bounds.y + margin,
+                  _bounds.width - (2 * margin),
+                  _bounds.height - (2 * margin)};
+}
+
+auto World::update(float time) -> void {
   _resources.update(time);
   _resources.feed_ants(_population);
   _population.update();
 }
 
-void World::draw() {
+auto World::draw() -> void {
   DrawRectangle(_bounds.x, _bounds.y, _bounds.width, _bounds.height, WHITE);
   _resources.draw();
   _population.draw();
+}
+
+auto World::out_of_bounds(const Rectangle& object) const -> bool {
+  return object.x < _bounds.x || object.y < _bounds.y ||
+         object.x + object.width > _bounds.x + _bounds.width ||
+         object.y + object.height > _bounds.y + _bounds.height;
 }
--- world.hpp
+++ world.hpp
@@ -16,13 +16,24 @@ class World {
 
   [[nodiscard]] auto get_bounds() const -> const Rectangle&;
 
-  void update(float time);
-  void draw();
+  [[nodiscard]] auto get_spawn_bounds() const -> const Rectangle&;
+  auto set_spawn_margin(float) -> void;
+  [[nodiscard]] auto get_spawn_margin() const -> float;
+
+  auto update(float time) -> void;
+  auto draw() -> void;
+
+  [[nodiscard]] auto out_of_bounds(const Rectangle& object) const -> bool;
 
  protected:
   const Rectangle DEFAULT_BOUNDS = {0.0f, 0.0f, 1000.0f, 1000.0f};
+  const float DEFAULT_SPAWN_MARGIN = 0.20F;
+
+  auto update_spawn_rect() -> void;
 
   Resources _resources;
   Population _population;
   Rectangle _bounds;
+  Rectangle _spawnBounds;
+  float _spawnMargin;
 };
