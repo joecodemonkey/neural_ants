7f76e5e0dfacb652ad1d0b6983d47e45c8811f37
Joe Hurdle <joehurdle@gmail.com>
2025-07-03 18:09:08 -0500

    Added new state: MEAN_FITNESS - also unit tests for state

--- include/ui/state.hpp
+++ include/ui/state.hpp
@@ -1,6 +1,4 @@
 #pragma once
-#include <imgui.h>
-#include <raylib.h>
 
 #include <unordered_set>
 
@@ -8,12 +6,13 @@ namespace UI {
 
 class State {
  public:
-  typedef enum Component { SETTINGS, PROGRESS, SAVELOAD } Component;
+  typedef enum Component { SETTINGS, MEAN_FITNESS, SAVELOAD } Component;
 
   State() = default;
   auto maximize(Component component) -> void;
   auto minimize(Component component) -> void;
   auto is_maximized(Component) -> bool;
+  auto toggle(Component component) -> void;
 
  protected:
   std::unordered_set<Component> _maximzedComponents;
